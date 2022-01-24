7a6ac12e8455af63ec311ce3b6c2432415dbbb6e
Marcus <marcusventin@gmail.com>
2022-01-24 16:43:19 +0000

    fixed broken tests

--- lib/space.rb
+++ lib/space.rb
@@ -3,21 +3,26 @@ require 'pg'
 class Space
   attr_reader :name, :description, :ppn
 
-def initialize (name:, description:, ppn:)
-  @name = name
-  @description = description
-  @ppn = ppn
-end
+  def initialize (id:, name:, description:, ppn:)
+    @id = id
+    @name = name
+    @description = description
+    @ppn = ppn
+  end
 
-def self.add(name:, description:, ppn:)
+  def self.add(name:, description:, ppn:)
 
-  if ENV['RACK_ENV'] == 'test'
-    connection = PG.connect(dbname: 'makersbnb_test')
-  else
-  connection = PG.connect(dbname: 'makersbnb')
-end
-  result = connection.exec("INSERT INTO spaces (name, description, ppn) VALUES (#{name}, #{description}, #{ppn}) RETURNING id, name, description, ppn;") 
-  Space.new(id:result[0]['id'],name:result[0]['name'], description:result[0]['description'], ppn:result[0]['ppn'])
-end
+    if ENV['RACK_ENV'] == 'test'
+      connection = PG.connect(dbname: 'makersbnb_test')
+    else
+      connection = PG.connect(dbname: 'makersbnb')
+    end
 
+    result = connection.exec_params(
+      'INSERT INTO spaces (name, description, ppn) VALUES ($1, $2, $3) 
+      RETURNING id, name, description, ppn;', [name, description, ppn.to_i])
+
+      Space.new(id: result[0]['id'], name: result[0]['name'], 
+      description: result[0]['description'], ppn: result[0]['ppn'])
+  end
 end
--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -7,7 +7,7 @@ describe Space do
       expect(test_space).to be_a Space
       expect(test_space.name).to eq 'test_property'
       expect(test_space.description).to eq 'test_description'
-      expect(test_space.ppn).to eq 100
+      expect(test_space.ppn).to eq '100'
     end
   end
 end
