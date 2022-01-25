afcf39207f0469f615dc78115fce7251a4f811b9
Marcus <marcusventin@gmail.com>
2022-01-25 16:18:30 +0000

    Panic over

--- lib/availability.rb
+++ lib/availability.rb
@@ -3,13 +3,12 @@ require 'date'
 class Availability
 
   def self.add_availability(name, start_date, end_date)
-    @dates = Date.parse(start_date)..Date.parse(end_date)
-    p @dates
+    @dates = (Date.parse(start_date) .. Date.parse(end_date)).map { |date| date.strftime("%F")}
 
     ENV['RACK_ENV'] == 'test' ? 
       connection = PG.connect(dbname: 'makersbnb_test')
       : connection = PG.connect(dbname: 'makersbnb')
 
-    @dates.each { |date| connection.exec_params("INSERT INTO availability (name, date) VALUES ($1, $2);" [name, Date.strptime(date, '%F')]) }
+    @dates.each { |date| connection.exec_params("INSERT INTO availability (name, date) VALUES ($1, $2);", [name, date]) }
   end
 end
--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -4,7 +4,7 @@ require_relative 'setup_test_database'
 describe Space do
   describe '.add' do
     it 'add space to the database' do
-      test_space = Space.add(name: 'test_property', description: 'test_description', ppn: 100, start_date: Date.parse(Date.today), end_date: Date.parse(Date.today+1))
+      test_space = Space.add(name: 'test_property', description: 'test_description', ppn: 100, start_date: '2022-02-02', end_date: '2022-04-06')
       expect(test_space).to be_a Space
       expect(test_space.name).to eq 'test_property'
       expect(test_space.description).to eq 'test_description'
@@ -15,8 +15,8 @@ describe Space do
   describe '.all' do
     it 'displays all spaces' do
       setup_test_database
-    test_space = Space.add(name: 'test_property', description: 'test_description', ppn: 100, start_date: Date.parse(Date.today), end_date: Date.parse(Date.today+1))
-    Space.add(name:'beach house', description:'on beach', ppn: 400, start_date: Date.parse(Date.today), end_date: Date.parse(Date.today+1))
+    test_space = Space.add(name: 'test_property', description: 'test_description', ppn: 100, start_date: '2022-02-02', end_date: '2022-04-06')
+    Space.add(name:'beach house', description:'on beach', ppn: 400, start_date: '2022-02-02', end_date: '2022-04-06')
     all_spaces = Space.all
     expect(all_spaces.first.name).to eq 'test_property'
     expect(all_spaces.last.description).to eq 'on beach'
