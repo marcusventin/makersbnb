176eb62fd4875cac5236ff97cbd3488313040461
OliviaMaxwell <olivia@maxwellnet.com>
2022-01-26 15:17:52 +0000

    Updated all 'user_name' to 'email'

--- lib/user.rb
+++ lib/user.rb
@@ -2,25 +2,23 @@ require 'pg'
 
 class User
 
-  attr_reader :user_id, :user_name, :password
+  attr_reader :user_id, :email , :password
 
-  def initialize(user_id:, user_name:, password:)
+  def initialize(user_id:, email:, password:)
     @user_id = user_id
-    @user_name = user_name
+    @email  = email 
     @password = password
   end
 
-  def self.sign_up(user_name:, password:)
+  def self.sign_up(email:, password:)
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'makersbnb_test')
     else
       connection = PG.connect(dbname: 'makersbnb')
     end
 
-    result = connection.exec_params("INSERT INTO users (user_name, password)
-    VALUES($1, $2) RETURNING user_name, password;", [user_name, password])
+    result = connection.exec_params("INSERT INTO users (email, password) VALUES($1, $2) RETURNING email , password;", [email , password])
 
-    User.new(user_id: result[0]['user_id'], user_name: result[0]['user_name'],
-    password: result[0]['password'])
+    User.new(user_id: result[0]['user_id'], email: result[0]['email'], password: result[0]['password'])
   end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -8,11 +8,11 @@ describe User do
     it 'creates an account for the user' do
       connection = PG.connect(dbname: 'makersbnb_test')
 
-      user1 = User.sign_up(user_name: 'Landlord', password: '123pass')
+      user1 = User.sign_up(email: 'landlord@rent.com', password: '123pass')
       # persisted_data = persisted_data(id: user1.id)
 
       expect(user1).to be_a User
-      expect(user1.user_name).to eq 'Landlord'
+      expect(user1.email).to eq 'landlord@rent.com'
       expect(user1.password).to eq '123pass'
 
     end
