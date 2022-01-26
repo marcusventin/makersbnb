90f52f510be3754e94c11bfd4855130fddfc979a
Marcus <marcusventin@gmail.com>
2022-01-26 09:38:59 +0000

    Added a truncate availability query to the setup test database method

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -2,6 +2,7 @@ require 'pg'
 
 def setup_test_database 
   connection = PG.connect(dbname: 'makersbnb_test')
-  connection.exec("TRUNCATE users;")
+  connection.exec('TRUNCATE users;')
   connection.exec('TRUNCATE spaces;')
+  connection.exec('TRUNCATE availability;')
 end
