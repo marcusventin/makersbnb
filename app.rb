8b44549c466215e21f6727c5878581eafbc1d1f8
Marcus <marcusventin@gmail.com>
2022-01-26 09:24:58 +0000

    standardised routes between the two branches

--- app.rb
+++ app.rb
@@ -10,33 +10,33 @@ class MakersBnB < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  get '/add' do
-    erb(:add)
-  end
-
-  post '/add' do
-    Space.add(name: params[:property_name], description: params[:description],
-      ppn: params[:ppn], start_date: params[:start_date], end_date: params[:end_date])
-    redirect '/add-confirmation'
-  end
-
-  get '/add-confirmation' do
-    @property = Space.all[0]
-    erb(:confirmation)
-  end
-
   get '/makersbnb' do
     erb :index
   end
-  
+
   get '/makersbnb/sign_up' do
     erb :sign_up
   end
 
-  post '/makersbnb' do
+  post '/makersbnb/sign_up' do
     User.sign_up(user_name: params[:user_name], password: params[:password])
     redirect '/makersbnb'
   end
-  
+
+  get '/makersbnb/add' do
+    erb(:add)
+  end
+
+  post '/makersbnb/add' do
+    Space.add(name: params[:property_name], description: params[:description],
+      ppn: params[:ppn], start_date: params[:start_date], end_date: params[:end_date])
+    redirect '/makersbnb/add-confirmation'
+  end
+
+  get '/makersbnb/add-confirmation' do
+    @property = Space.all[0]
+    erb(:confirmation)
+  end
+
   run! if app_file == $PROGRAM_NAME
 end
